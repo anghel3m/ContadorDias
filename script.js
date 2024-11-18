3c0cfae742056c9c27436b8f708d08473a25d593
Anghel3m <adms95@outlook.es>
2024-11-17 20:57:23 -0500

    arreglos moviles

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ function agregaFechaAudiencia() {
       document.getElementById("FechaAudiencia").appendChild(nuevoDiv); 
       } 
 
+
+
+
+      function calcularDiasTranscurridos(fechaInicio, fechaFin) 
+      { 
+      const fecha1 = new Date(fechaInicio); const fecha2 = new Date(fechaFin); 
+      
+      const diferencia = fecha2 - fecha1; 
+      
+      const diasTranscurridos = Math.ceil(diferencia / (1000 * 60 * 60 * 24)); 
+      return diasTranscurridos; } 
+       
+       
+       function compararFechas() 
+       { 
+        const fechaInicio = document.getElementById(idFechaInicio).value; 
+       
+       const fechaFin = document.getElementById("fechaFin").value;
+        if (fechaInicio && fechaFin) 
+         {
+         const dias = calcularDiasTranscurridos(fechaInicio, fechaFin); 
+         document.getElementById("resultado").innerText = "Días transcurridos: " + dias;
+          } else { 
+          document.getElementById("resultado").innerText = "Por favor, seleccione ambas fechas."; 
+         } 
+         }
